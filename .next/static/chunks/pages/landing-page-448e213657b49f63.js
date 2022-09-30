18f42a947832b19cdb3e4e20bf59b63084a1322c
Beatriz <biaperon2001@gmail.com>
2022-09-30 09:51:57 -0300

    Apagando arquivos desnecessários

--- .next/static/chunks/pages/landing-page-448e213657b49f63.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[545],{7109:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/landing-page",function(){return u(9467)}])}},function(n){n.O(0,[664,13,467,774,888,179],(function(){return _=7109,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
